03ab7e2afc17e33993ae29495325270856a0344b
tsukanov-as <tsukanov.as@yandex.ru>
2021-05-31 22:35:08 +0300

    refactoring

---
+++ python/bench.py
@@ -0,0 +1,27 @@
+import abc_dsl as abc
+
+import time
+
+start = time.time()
+
+it = abc.Model()
+
+it.quacks = it.quacks
+it.flies = it.flies
+it.swims = it.swims
+it.croaks = it.croaks
+
+for i in range(1_000_000):
+    it.duck[i] = it.quacks & (it.flies | it.swims)
+
+tick = abc.Build(it)
+
+print("compilation time: ", time.time() - start)
+
+tick()
+
+start = time.time()
+
+tick()
+
+print("calculation time: ", time.time()-start)
---
+++ python/example.py
@@ -0,0 +1,30 @@
+import abc_dsl as abc
+
+m = abc.Model()
+
+quacks = m.quacks(True)
+flies = m.flies(True)
+swims = m.swims(True)
+croaks = m.croaks(True)
+
+m.duck = m.quacks & (m.flies | m.swims)
+m.frog = m.croaks & m.swims & ~m.flies
+
+tick = abc.Build(m)
+
+state = tick()
+
+print(state)
+
+state[quacks] = 0
+state[flies] = 0
+state[swims] = 1
+state[croaks] = 1
+
+print(state)
+
+state = tick()
+print(state)
+
+print("is it a duck?", state[m.duck()])
+print("is it a frog?", state[m.frog()])
